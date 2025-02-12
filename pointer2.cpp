1ea4153b81d5cc51e1ec3d4ab4195868fe65c7f5
Dhruv-AFK <dhruvjha1239@gmail.com>
2025-02-12 21:18:03 +0530

    character array

--- pointer2.cpp
+++ pointer2.cpp
@@ -5,6 +5,17 @@ int main (){
 
 
     int arr[10] = { 1,2,3,4,5};
+    char ch[60] = "a b c d e ";
+
+    int *ptr = &arr[0];
+    char *c = &ch[0];
+
+    cout << ptr << endl;
+
+    cout << *ptr << endl;
+
+    cout<< c << endl;
+    cout<< *c << endl;
 
     
 }
