424e64b176110ac8a4d4e2941530a8d77a8e6554
Dhruv-AFK <dhruvjha1239@gmail.com>
2025-02-12 23:37:06 +0530

    playing with double

--- doublepointer.cpp
+++ doublepointer.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 using namespace std ;
 
+int main (){
+
+    int i = 5 ;
+    int* p = &i;
+    int** p2 = &p;
+    /*
+    cout << p << endl;
+    cout << p2 << endl;
+
+    cout << *p << endl;
+    cout << **p2 << endl;
+
+    cout << *p2 << endl;
+    */
+
+    cout << i << endl;
+    cout << *p << endl;
+    cout << **p2 <<endl;
+}
